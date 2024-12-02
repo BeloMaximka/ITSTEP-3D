bc7137e1f2745b5496b286bbb0610599ef3d1097
BeloMaximka <belomaxim@gmail.com>
2024-12-02 10:11:18 +0200

    Save/cancel settings

---
+++ Assets/Scripts/Enums/SettingKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Enums
+{
+    public static class SettingKey
+    {
+        public const string EffectsVolume = "EffectsVolume";
+        public const string MusicVolume = "MusicVolume";
+        public const string IsMuted = "IsMuted";
+        public const string MouseSensitivityX = "MouseSensitivityX";
+        public const string MouseSensitivityY = "MouseSensitivityY";
+        public const string IsMouseSensitivityShared = "IsMouseSensitivityShared";
+    }
+}
--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts;
+using Assets.Scripts.Enums;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,12 +13,27 @@ public class SettingsScript : MonoBehaviour
 
     [SerializeField]
     private Slider sensitivityYSlider;
+
+    [SerializeField]
+    private Toggle sharedSensitivityToggle;
+
+    [SerializeField]
+    private Slider effectSoundsSlider;
+
+    [SerializeField]
+    private Slider musicSoundsSlider;
+
+    [SerializeField]
+    private Toggle muteSoundsToggle;
+
     void Start()
     {
         content = transform.Find("Content").gameObject;
         content.SetActive(false);
 
         OnSensitivitySeparateChanged(GameSettings.IsMouseSensitivityShared);
+        OnCancel();
+        SaveSettings();
     }
 
     // Update is called once per frame
@@ -66,6 +82,72 @@ public class SettingsScript : MonoBehaviour
         if (GameSettings.IsMouseSensitivityShared)
         {
             sensitivityYSlider.value = sensitivityXSlider.value;
+        }
+    }
+
+    public void OnCancel()
+    {
+        LoadSettings();
+
+        effectSoundsSlider.value = GameSettings.EffectsVolume;
+        musicSoundsSlider.value = GameSettings.MusicVolume;
+        muteSoundsToggle.isOn = GameSettings.IsMuted;
+        sensitivityXSlider.value = GameSettings.MouseSensitivityX;
+        sensitivityYSlider.value = GameSettings.MouseSensitivityY;
+        sharedSensitivityToggle.isOn = GameSettings.IsMouseSensitivityShared;
+    }
+
+    public void OnSave()
+    {
+        SaveSettings();
+    }
+
+    public void OnClose()
+    {
+        content.SetActive(false);
+    }
+
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetFloat(SettingKey.EffectsVolume, GameSettings.EffectsVolume);
+        PlayerPrefs.SetFloat(SettingKey.MusicVolume, GameSettings.MusicVolume);
+        PlayerPrefs.SetInt(SettingKey.IsMuted, Convert.ToInt32(GameSettings.IsMuted));
+        PlayerPrefs.SetFloat(SettingKey.MouseSensitivityX, GameSettings.MouseSensitivityX);
+        PlayerPrefs.SetFloat(SettingKey.MouseSensitivityY, GameSettings.MouseSensitivityY);
+        PlayerPrefs.SetInt(SettingKey.IsMouseSensitivityShared, Convert.ToInt32(GameSettings.IsMouseSensitivityShared));
+        PlayerPrefs.Save();
+    }
+
+    private void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey(SettingKey.EffectsVolume))
+        {
+            GameSettings.EffectsVolume = PlayerPrefs.GetFloat(SettingKey.EffectsVolume);
+        }
+
+        if (PlayerPrefs.HasKey(SettingKey.MusicVolume))
+        {
+            GameSettings.MusicVolume = PlayerPrefs.GetFloat(SettingKey.MusicVolume);
+        }
+
+        if (PlayerPrefs.HasKey(SettingKey.IsMuted))
+        {
+            GameSettings.IsMuted = PlayerPrefs.GetInt(SettingKey.IsMuted) == 1;
+        }
+
+        if (PlayerPrefs.HasKey(SettingKey.MouseSensitivityX))
+        {
+            GameSettings.MouseSensitivityX = PlayerPrefs.GetFloat(SettingKey.MouseSensitivityX);
+        }
+
+        if (PlayerPrefs.HasKey(SettingKey.MouseSensitivityY))
+        {
+            GameSettings.MouseSensitivityY = PlayerPrefs.GetFloat(SettingKey.MouseSensitivityY);
+        }
+
+        if (PlayerPrefs.HasKey(SettingKey.IsMouseSensitivityShared))
+        {
+            GameSettings.IsMouseSensitivityShared = PlayerPrefs.GetInt(SettingKey.IsMouseSensitivityShared) == 1;
         }
     }
 }
